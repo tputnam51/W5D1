7ef606bde10e34f65771b7986c04946b918abbde
tahj <tahjharris00@gmail.com>
2021-03-22 14:06:24 -0500

    monkey patch hash methods for array and string class

--- HashMap/lib/p02_hashing.rb
+++ HashMap/lib/p02_hashing.rb
@@ -4,11 +4,19 @@ end
 
 class Array
   def hash
+    
+    self.join.to_i.to_s(2).to_i
+    # self.each_with_index do |(ele, i)|
+    #   (ele.hash, i.hash)
+
+    # end
   end
 end
 
 class String
   def hash
+    arr = self.split("").map {|char| char.ord}
+    arr.join.to_id
   end
 end
 
@@ -16,6 +24,7 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
+
     0
   end
 end
