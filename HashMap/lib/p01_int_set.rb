f41586301189d334326ceefc4c9f5ce0485dd164
tahj <tahjharris00@gmail.com>
2021-03-22 10:58:15 -0500

    create IntSet

--- HashMap/lib/p01_int_set.rb
+++ HashMap/lib/p01_int_set.rb
@@ -11,6 +11,7 @@ class MaxIntSet
   end
 
   def remove(num)
+      @store[num] = false
   end
 
   def include?(num)
@@ -34,18 +35,23 @@ class IntSet
   end
 
   def insert(num)
+    return false if include?(num)
+    self[num] << num
+    return num
   end
 
   def remove(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
